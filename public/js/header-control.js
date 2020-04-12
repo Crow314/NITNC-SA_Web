6fe4e3d4aca4d2178e10af4931a7edadef07fda0
Crow314 <master@crow31415.net>
2020-04-13 05:26:40 +0900

    コメント💢💢💢

--- public/js/header-control.js
+++ public/js/header-control.js
@@ -4,20 +4,22 @@ $(function(){
         const targetId = $(this).attr('id').replace('list', 'accordion');
         const $target = $headerAccordion.find('#' + targetId);
         const $currentOpened = $headerAccordion.find('.show');
-        if($currentOpened.length === 0){
+
+        if($currentOpened.length === 0){ //アコーディオン非表示時
+            //アコーディオン展開
             $headerAccordion.slideDown();
             $target.show();
 
             $(this).addClass('header-list-selected');
             $target.addClass('show');
-        }else {
-            if($target.hasClass('show')) {
+        }else { //アコーディオン表示時
+            if($target.hasClass('show')) {//アコーディオン縮小
                 $headerAccordion.slideUp();
                 $target.hide();
 
                 $(this).removeClass('header-list-selected');
                 $target.removeClass('show');
-            }else {
+            }else {//アコーディオン内容切り替え
                 $currentOpened.hide();
                 $target.show();
 
