8a1e89aa667f9fd133079fc673373d5237527638
Crow314 <master@crow31415.net>
2020-05-10 04:48:11 +0900

    金額計算機能実装

--- public/tool/payment-application/form.js
+++ public/tool/payment-application/form.js
@@ -1,4 +1,5 @@
 $(function () {
+    // 行追加
     $('.add-row').click(function () {
         const $tbody = $(this).parents('table').children('tbody');
         const $target = $tbody.children('tr:last');
@@ -11,7 +12,8 @@ $(function () {
         $tbody.append($clone);
     });
 
-    $(document).on('click','.remove-row', function () {
+    // 行削除
+    $('table').on('click','.remove-row', function () {
         const $target = $(this).parents('tr');
         const $indexFixTargets = $target.nextAll('tr');
         let $index = $target.children('.index');
@@ -20,9 +22,66 @@ $(function () {
         $target.remove();
 
         //削除行以降インデックス調整
-        $indexFixTargets.each(function(index, element){
+        $indexFixTargets.each(function (index, element) {
             $(element).children('.index').text(indexNum);
             indexNum++;
         })
     });
+
+    // 金額計算系イベント設定
+    $('tbody').on('change focus blur', "input[name='quantity']", amountCalcFunction)
+        .on('change focus blur', "input[name='unit_price']", amountCalcFunction)
+        .on('change focus blur', "input[name='quantity']", totalAmountCalc)
+        .on('change focus blur', "input[name='unit_price']", totalAmountCalc)
+        .on('change', "select[name='tax_rate']", totalAmountCalc);
+
 });
+
+// 各行金額計算
+function amountCalcFunction() {
+    const $row = $(this).parents('tr');
+    const $quantity = $row.find("input[name='quantity']");
+    const $unitPrice = $row.find("input[name='unit_price']");
+    const $amount = $row.find('.amount');
+
+    const quantity = nanToZero(parseInt($quantity.val()));
+    const unitPrice = nanToZero(parseInt($unitPrice.val()));
+
+    const amount = quantity * unitPrice;
+    $amount.text(thousandSeparate(amount));
+}
+
+// 合計額計算
+function totalAmountCalc() {
+    const $rows = $(this).parents('tbody').children('tr');
+    const $totalAmount = $rows.parents('table').find('.total-amount');
+
+    let totalAmount = 0;
+    $rows.each(function (index, element) {
+        const $amount = $(element).find('.amount');
+        const $taxRate = $(element).find("select[name='tax_rate']");
+
+        const amount = parseInt(removeComma($amount.text()));
+        const taxRate = parseInt($taxRate.val());
+
+        totalAmount += amount * (taxRate*0.01 + 1);
+    });
+
+    $totalAmount.text(thousandSeparate(Math.floor(totalAmount))); //小数切り捨て&コンマ区切り
+}
+
+
+function nanToZero(number) {
+    if(isNaN(Number(number))) {
+        number = 0;
+    }
+    return number;
+}
+
+function thousandSeparate(number) {
+    return String(number).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
+
+function removeComma(number) {
+    return String(number).replace(/,/g, '');
+}
