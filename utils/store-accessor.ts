7e0d608a1cde9603e71cf3f7b46a7ca9b8555b2e
Crow314 <master@crow31415.net>
2021-09-04 04:53:41 +0900

    Setup Vuex

---
+++ utils/store-accessor.ts
@@ -0,0 +1,12 @@
+import { Store } from 'vuex'
+import { getModule } from 'vuex-module-decorators'
+import navigation from '@/store/navigation'
+
+// eslint-disable-next-line import/no-mutable-exports
+let navigationStore: navigation
+
+function initialiseStores (store: Store<any>): void {
+  navigationStore = getModule(navigation, store)
+}
+
+export { initialiseStores, navigationStore }
