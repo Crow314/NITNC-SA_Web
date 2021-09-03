2454661f575282fd74aec3fbb8776a6eae239686
Crow314 <master@crow31415.net>
2021-09-03 22:18:12 +0900

    Add plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,18 @@
 export default {
-  buildModules: ['@nuxt/typescript-build']
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/vuetify'
+  ],
+  buildModules: [
+    '@nuxtjs/composition-api/module',
+    '@nuxtjs/eslint-module',
+    '@nuxt/typescript-build'
+  ],
+  transpileDependencies: [
+    'vuex-module-decorators'
+  ],
+  plugins: [
+    '@/plugins/axios.js'
+  ],
+  components: true
 }
