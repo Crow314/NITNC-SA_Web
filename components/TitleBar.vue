85c0834f1cfe5f4f80c1dce5abcc0d3c0da17729
Crow314 <master@crow31415.net>
2021-09-05 19:18:41 +0900

    Change title in TitleBar.vue to property

--- components/TitleBar.vue
+++ components/TitleBar.vue
@@ -2,17 +2,20 @@
   <div>
     <v-app-bar app dark elevate-on-scroll :clipped-left="navigationClipped">
       <v-app-bar-nav-icon @click="toggleNavigationClipped" />
-      <v-toolbar-title>NITNC-SA</v-toolbar-title>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-app-bar>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
 import { navigationStore } from '@/store'
 
 @Component
 export default class TitleBar extends Vue {
+  @Prop({ type: String, default: 'NITNC-SA' })
+  title!: string;
+
   get navigationClipped(): boolean {
     return navigationStore.isClipped
   }
